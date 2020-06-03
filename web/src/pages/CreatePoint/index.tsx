e50be7c593380b2550fee232beec72fb67042035
Ariel Kollross <arielkollross@gmail.com>
2020-06-03 17:50:33 -0300

    consult IBGE API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import {Map, TileLayer, Marker } from 'react-leaflet'
+import React, { useEffect, useState, ChangeEvent} from 'react';
+import { Link } from 'react-router-dom';
+import {Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+import axios from 'axios';
 
 import './CreatePoint.css';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 import { FiArrowDownLeft } from 'react-icons/fi';
 
+
+//when create array or object, need to informr manually this type.
+
+interface Item{
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface IGBGE_UfResponse {
+    sigla: string;
+}
+
+interface IBGE_CityResonse {
+    nome: string;
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
+
+    useEffect(() => {
+        api.get('items').then(res => setItems(res.data))
+    } , []);
+
+    useEffect(() => {
+        axios.get<IGBGE_UfResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then(res => {
+            const ufInitials = res.data.map( uf => uf.sigla)
+
+            setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        //new load the UF always change UF 
+        if(selectedUf == '0'){
+            return;
+        }
+
+        axios.
+        get<IBGE_CityResonse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(res => {
+            const cityNames = res.data.map( city => city.nome)
+    
+            setCities(cityNames);
+        });
+        
+    }, [selectedUf])
+
+    function handleSelecttUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value;
+
+        setSelectedUf(uf);
+    }
+
+    function handleSelecttCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = event.target.value;
+
+        setSelectedCity(city);
+    }
+
     return (
         <div id="page-create-point">
             <header>
@@ -75,15 +143,29 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select name="uf"
+                            id="uf"
+                            value={selectedUf}
+                            onChange={handleSelecttUf}
+                            >
                                 <option value="0">Selecione um UF</option>
+                                {ufs.map( uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select name="city"
+                             id="city"
+                            value={selectedCity}
+                            onChange={handleSelecttCity}
+                            >
                                 <option value="0">Selecione uma cidade</option>
+                                {cities.map( city => (
+                                <option key={city} value={city}>{city}</option>
+                                ))}
                             </select>
                         </div>
 
@@ -97,35 +179,13 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
+                        {items.map(item => (
+                        <li key={item.id}>
+                            <img src={item.image_url} alt={item.title}/>
+                            <span>{item.title}</span>
                         </li>
 
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        ))}
                     </ul>
 
                 </fieldset>
