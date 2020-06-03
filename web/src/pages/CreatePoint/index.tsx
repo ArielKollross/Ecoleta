3263a6190aa0b097298bd079bf5064674e2d3bde
Ariel Kollross <arielkollross@gmail.com>
2020-06-03 14:07:53 -0300

    inclued map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import {Map, TileLayer, Marker } from 'react-leaflet'
 
 import './CreatePoint.css';
 import logo from '../../assets/logo.svg'
@@ -60,6 +61,16 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+
+                    <Map center={[-27.209205, -49.6401092]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-27.209205, -49.6401092]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
